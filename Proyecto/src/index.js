679b08c91fa7d72f1355c98d5d87e34267dbcddb
JohnJairoGonzalezMartinez <johngonzalez@javeriana.edu.co>
2021-10-09 17:06:38 -0500

    Final

--- Proyecto/src/index.js
+++ Proyecto/src/index.js
@@ -13,35 +13,42 @@ server.listen(3000, () => {
 
 app.use( express.json() )
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+const { Board, Sensor } = require("johnny-five");
+const board = new Board();
 
+board.on("ready", () => {
+    const potentiometer = new Sensor({
+        pin: "A3",
+        freq: 1000
+    });     
 
+    let lastValue = 0
 
-
-
-app.post( "/move", (req, res) =>{
-    io.emit('move', req.body); 
-    res.send('ok')
-})
+    potentiometer.on("data", async () => { 
+        const value = potentiometer.scaleTo(-10, 10)
+        io.emit('move', {
+            movement: value * 0.02,
+        });
+        console.log( "Readed Value:", value )
+  });
+});
 
 app.get( "/",  (req, res) => {    
-    res.sendFile('/index.html' , { root: "./src/"})
+    res.sendFile('index.html' , { root: "./src/serve/"})
 })
 
 app.get( "/OrbitControls.js", (req, res) =>{
-    res.sendFile('/OrbitControls.js' , { root: "./src/"})
+    res.sendFile('/OrbitControls.js' , { root: "./src/serve/"})
 })
 
 app.get( "/three.module.js", (req, res) =>{
-    res.sendFile('/three.module.js' , { root: "./src/"})
+    res.sendFile('/three.module.js' , { root: "./src/serve/"})
 })
 
 app.get( "/three.module.js", (req, res) =>{
-    res.sendFile('/three.module.js' , { root: "./src/"})
+    res.sendFile('/three.module.js' , { root: "./src/serve/"})
 })
 
 app.get( "/img1.jpg", (req, res) =>{
-    res.sendFile('/img1.jpg' , { root: "./src/"})
+    res.sendFile('/img1.jpg' , { root: "./src/serve/"})
 })
