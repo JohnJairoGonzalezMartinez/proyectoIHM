ac56f11c19f55f981bdcf077f46b32942390bc3b
JohnJairoGonzalezMartinez <johngonzalez@javeriana.edu.co>
2021-10-12 17:54:06 -0500

    Parcial 2

---
+++ Parcial/src/index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+io.on('connection', (socket) => {});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000')
+})
+
+app.use( express.json() )
+
+const { Board, Sensor } = require("johnny-five");
+const board = new Board();
+
+board.on("ready", () => {
+    const rotationPot = new Sensor({pin: "A3",freq: 1000});
+    const sizePot = new Sensor({pin: "A4",freq: 1000});
+
+    rotationPot.on("data", async () => { 
+        const value = rotationPot.scaleTo(-10, 10)
+        console.log( 'ROTATION POT : ', value );
+        io.emit('move', {
+            movement: value * 0.02,
+        });
+
+    });
+
+    sizePot.on("data", async () => { 
+        const value = sizePot.scaleTo(-10, 10)
+        console.log( 'SIZE POT : ', value );
+        io.emit('size', {
+            scale: value * 0.05,
+        });
+
+    });
+
+});
+
+var keypress = require('keypress');
+keypress(process.stdin);
+process.stdin.on('keypress', function (ch, key) {
+    let texture = ''
+    switch(key.name){
+        case 'up': texture = 'src/img1.jpg'; break;
+        case 'down': texture = 'src/img2.jpg'; break;
+        case 'left': texture = 'src/img3.jpg'; break;
+        case 'right': texture = 'src/img4.jpg'; break;
+        default: return
+    }
+    io.emit('texture', {
+        texture
+    })
+});
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+app.get( "/",  (req, res) => {    
+    res.sendFile('index.html' , { root: "./src/serve/"})
+})
+
+app.get( "/src/:source", (req, res) =>{
+    const {source} = req.params
+    res.sendFile(source , { root: "./src/serve/"})
+})
